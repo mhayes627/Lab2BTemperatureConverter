bbc330eb312dada5c2f6a2e103a646067b58290d
mhayes627 <mhayes6@stu.jsu.edu>
2024-02-05 17:03:51 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/lab2btemperatureconverter/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lab2btemperatureconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.lab2btemperatureconverter.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
