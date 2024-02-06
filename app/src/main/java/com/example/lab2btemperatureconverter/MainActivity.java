c4739d437666ec5636a6a0aff5d9338a66d69374
mhayes627 <mhayes6@stu.jsu.edu>
2024-02-05 19:08:31 -0600

    Completed conversion app

--- app/src/main/java/com/example/lab2btemperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/lab2btemperatureconverter/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.lab2btemperatureconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 
 import com.example.lab2btemperatureconverter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+    double f_temp, c_temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,41 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isvalidConversion()){
+                    convert();
+                }
+            }
+        });
+    }
+
+    protected boolean isvalidConversion(){
+        if(binding.fInput.getText().toString().equals(".") || binding.cInput.getText().toString().equals(".")){
+            return false;
+        }
+        else if (binding.fInput.getText().toString().isEmpty() && binding.cInput.getText().toString().isEmpty()){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    protected void convert(){
+
+        if (TextUtils.isEmpty(binding.fInput.getText())){
+            c_temp = Double.parseDouble(binding.cInput.getText().toString());
+            f_temp = (c_temp * 9/5) + 32;
+            binding.fInput.setText(String.valueOf(f_temp));
+        }
+        else {
+            f_temp = Double.parseDouble(binding.fInput.getText().toString());
+            c_temp = (f_temp - 32) * 5/9;
+            binding.cInput.setText(String.valueOf(c_temp));
+        }
+
+
     }
 }
